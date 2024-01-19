6e18eff509ef8fc3e068e7b048a64a811f42b870
engZena <zena_al_ja95@yahoo.com>
2024-01-19 22:32:31 +0300

    create review model

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -4,44 +4,44 @@ const reviewSchema = new mongoose.Schema(
   {
     review: {
       type: String,
-      required: [true, 'Review can not be empty!']
+      required: [true, "Review can not be empty!"],
     },
     rating: {
       type: Number,
       min: 1,
-      max: 5
+      max: 5,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     book: {
       type: mongoose.Schema.ObjectId,
-      ref: 'Book',
-      required: [true, 'Review must belong to a book.']
+      ref: "Book",
+      required: [true, "Review must belong to a book."],
     },
     user: {
       type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: [true, 'Review must belong to a user']
-    }
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
 );
 
 reviewSchema.index({ book: 1, user: 1 }, { unique: true });
 
-reviewSchema.pre(/^find/, function(next) {
+reviewSchema.pre(/^find/, function (next) {
   this.populate({
-    path: 'user',
-    select: 'name'
+    path: "user",
+    select: "name",
   });
   next();
 });
 
-const Review = mongoose.model('Review', reviewSchema);
+const Review = mongoose.model("Review", reviewSchema);
 
 export default Review;
