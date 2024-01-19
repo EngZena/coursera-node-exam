fe0654202592fb48676b81bca240ab38cd1d3822
engZena <zena_al_ja95@yahoo.com>
2024-01-19 22:28:26 +0300

    load config variable using dotenv library

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -73,7 +73,7 @@ export const protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
+  } else if (req.cookies && req.cookies.jwt) {
     token = req.cookies.jwt;
   }
 
@@ -82,7 +82,7 @@ export const protect = catchAsync(async (req, res, next) => {
       new AppError('Your are not logged in!, Please log in to get access ', 401)
     );
   }
-  const decoded = await promisify(jwt.verify)(token, config.get('JWT_SECRET'));
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   const freshUser = await User.findById(decoded.id);
   if (!freshUser) {
     return next(new AppError('The user is no longer exist', 401));
